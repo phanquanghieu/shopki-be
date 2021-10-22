e1ae56b12d3bd2fa645c86846194fa23a92394d4
chinhcd <7a3dqh@gmail.com>
2021-10-22 17:10:54 +0700

    s

--- src/main/java/com/laptrinhweb/shopkibe/controller/ShopController.java
+++ src/main/java/com/laptrinhweb/shopkibe/controller/ShopController.java
@@ -1,10 +1,10 @@
 package com.laptrinhweb.shopkibe.controller;
 
 import com.laptrinhweb.shopkibe.dtos.ShopDTO;
+import com.laptrinhweb.shopkibe.payload.ApiResponse;
 import com.laptrinhweb.shopkibe.responses.ShopResponse;
 import com.laptrinhweb.shopkibe.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,5 +16,10 @@ public class ShopController {
     @GetMapping()
     public ShopResponse getShop(@RequestParam String id){
         return shopService.getShop(id);
+    }
+
+    @PostMapping("/edit")
+    public ApiResponse editShop(@RequestBody ShopDTO shopDTO){
+        return shopService.editShop(shopDTO);
     }
 }
